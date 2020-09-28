5f852646107f6d27426b84d01f949d15875e785f
hong456 <l54755611@gmail.com>
2020-09-28 15:57:45 +0800

    簡易開關警報，接線發起警報

--- SmartHome/SmartHome.ino
+++ SmartHome/SmartHome.ino
@@ -2,13 +2,24 @@ void setup()
 {
 pinMode(12, OUTPUT);
 pinMode(11, OUTPUT);
+pinMode(7, OUTPUT);
+pinMode(2, INPUT);
+Serial.begin(9600);
 }
 void loop()
 {
+if(digitalRead(2)==HIGH) {
+Serial.println("HIGH");
+}
+else {
+Serial.println("LOW");
+}
 digitalWrite(12, HIGH);
 digitalWrite(11, LOW);
+tone(7, 1000, 500);
 delay(500);
 digitalWrite(12, LOW);
 digitalWrite(11, HIGH);
+tone(7, 800, 500);
 delay(500);
 }
