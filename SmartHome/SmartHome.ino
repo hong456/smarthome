93c6b0b9aabe5eb51015d9b92ee90d63fbfc00fa
hong456 <l54755611@gmail.com>
2020-09-28 16:18:23 +0800

    修改成斷線後才發警報

--- SmartHome/SmartHome.ino
+++ SmartHome/SmartHome.ino
@@ -9,11 +9,6 @@ Serial.begin(9600);
 void loop()
 {
 if(digitalRead(2)==HIGH) {
-Serial.println("HIGH");
-}
-else {
-Serial.println("LOW");
-}
 digitalWrite(12, HIGH);
 digitalWrite(11, LOW);
 tone(7, 1000, 500);
@@ -22,4 +17,5 @@ digitalWrite(12, LOW);
 digitalWrite(11, HIGH);
 tone(7, 800, 500);
 delay(500);
+}
 }
